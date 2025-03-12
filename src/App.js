e79c7ab292a5a1aef357bf2a42cea2fb3c8c04d2
TylerMcKell-DOMO <tyler.mckell@domo.com>
2025-03-11 20:47:43 -0600

    starts to add functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
+import './styles/App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [allTasks] = useState([]);
+  const [taskName, setTaskName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`the name you entered was: ${taskName}`);
+
+    setTaskName("");
+  }
+
   return (
     <div className="App">
-this is the to do list
+      this is the to do list app
+
+      <div className='inputs-wrapper'>
+        <form onSubmit={handleSubmit}>
+
+          <label>
+            new task name:
+            <input 
+              type='text' 
+              value={taskName}
+              onChange={(e) => setTaskName(e.target.value)}
+            />
+          </label>
+          <input type='submit' />
+        </form>
+
+      </div>
     </div>
   );
 }
